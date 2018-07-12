5899e7ae1f5fe105c3b12ada43b39b8efe4034bf
jeremy hennebique <jimimi@hotmail.fr>
2018-07-12 13:23:25 +0200

    enregistrement du pseudo si erreur de post

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -19,7 +19,7 @@
 <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
     <div>
         <label for="author">Auteur</label><br />
-        <input type="text" id="author" name="author" />
+        <input type="text" id="author" name="author" value="<?php if(isset($author)) echo $author ?>" />
     </div>
     <div>
         <label for="comment">Commentaire</label><br />
